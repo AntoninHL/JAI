6acff47a10e9431e89af7606a207f6c1a9a82ed9
Antonin <antonin@libfy.com>
2024-08-08 15:50:49 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
 
+# -- Project information -----------------------------------------------------
+project = 'JAI'
+copyright = '2024, SIT'
+author = 'AH'
+release = '3.7.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
